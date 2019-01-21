28e5cd1ad3c32b8ae49bb992af2e39ecdf019608
R. Stephen Guerra <rguerra712@yahoo.com>
2019-01-21 12:24:30 -0600

    Fixing delay by fixing bitmap to be micro

--- app/src/main/java/com/ap/stephen/videodrawerplayer/content/VideoItem.java
+++ app/src/main/java/com/ap/stephen/videodrawerplayer/content/VideoItem.java
@@ -1,16 +1,19 @@
 package com.ap.stephen.videodrawerplayer.content;
 
 import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.provider.MediaStore;
+
+import java.util.HashMap;
 
 public class VideoItem {
     private final String name;
     private final String path;
-    private final Bitmap bitmap;
+    private final static HashMap<String, Bitmap> BitmapCache = new HashMap<>();
 
-    public VideoItem(String name, String path, Bitmap bitmap) {
+    public VideoItem(String name, String path) {
         this.name = name;
         this.path = path;
-        this.bitmap = bitmap;
     }
 
     @Override
@@ -27,6 +30,16 @@ public class VideoItem {
     }
 
     public Bitmap getBitmap() {
-        return bitmap;
+        return buildBitmap();
+    }
+
+    private Bitmap buildBitmap() {
+        String path = getPath();
+        if (!BitmapCache.containsKey(path)) {
+            Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(getPath(),
+                    MediaStore.Video.Thumbnails.MICRO_KIND);
+            BitmapCache.put(path, bitmap);
+        }
+        return BitmapCache.get(path);
     }
 }
--- app/src/main/java/com/ap/stephen/videodrawerplayer/repositories/SdCardVideoRepository.java
+++ app/src/main/java/com/ap/stephen/videodrawerplayer/repositories/SdCardVideoRepository.java
@@ -1,9 +1,6 @@
 package com.ap.stephen.videodrawerplayer.repositories;
 
-import android.graphics.Bitmap;
-import android.media.ThumbnailUtils;
 import android.os.Environment;
-import android.provider.MediaStore;
 
 import com.ap.stephen.videodrawerplayer.content.VideoItem;
 
@@ -21,29 +18,40 @@ public class SdCardVideoRepository {
     private List<VideoItem> items;
     private Map<String, VideoItem> itemMap;
 
+    private Object itemsLoadLock = new Object();
     public List<VideoItem> getItems() {
         if (items == null) {
-            setItemsAndMapFrom(loadItemsFromMoviesFolder());
+            synchronized (itemsLoadLock)
+            {
+                if (items == null) {
+                    setItemsAndMapFrom(loadItemsFromMoviesFolder());
+                }
+            }
         }
         return items;
     }
+    private Object itemMapLoadLock = new Object();
     public Map<String, VideoItem> getItemMap() {
         if (itemMap == null) {
-            setItemsAndMapFrom(loadItemsFromMoviesFolder());
+            synchronized (itemMapLoadLock) {
+                if (itemMap == null) {
+                    setItemsAndMapFrom(loadItemsFromMoviesFolder());
+                }
+            }
         }
         return itemMap;
     }
 
     protected List<VideoItem> loadItemsFromMoviesFolder() {
-        String[] externalStorageDirectories = new String[] { Environment.getExternalStorageDirectory().toString(), "/mnt/sdcard2"};
         List<VideoItem> itemsLoaded = new ArrayList<>();
+
+        String[] externalStorageDirectories = new String[]{Environment.getExternalStorageDirectory().toString(), "/mnt/sdcard2"};
         for (String externalStorageDirectory : externalStorageDirectories) {
             String pathName = externalStorageDirectory + "/movies";
             File file = new File(pathName);
             File list[] = file.listFiles();
             if (list == null) continue;
-            for( int i=0; i< list.length; i++)
-            {
+            for (int i = 0; i < list.length; i++) {
                 VideoItem item = buildItem(list[i].getName(), pathName + "/" + list[i].getName());
                 itemsLoaded.add(item);
             }
@@ -56,10 +64,8 @@ public class SdCardVideoRepository {
     }
 
     private static VideoItem buildItem(String name, String videoFilePath) {
-        Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(videoFilePath,
-                MediaStore.Video.Thumbnails.MINI_KIND);
         name = name.replace(".mp4", "");
-        return new VideoItem(name, videoFilePath, bitmap);
+        return new VideoItem(name, videoFilePath);
     }
 
 
